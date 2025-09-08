7f06a97ab4ad631219d1bdb7db1dc4a0cc909652
rpaiello <rpaiello@gmail.com>
2025-09-08 12:59:09 -0400

    Random index text ("random.js") + updated placeholders

---
+++ random.js
@@ -0,0 +1,31 @@
+ const textSelect= [
+  '"It\'s far more impressive and personable than YOUR website!"',
+  '<b>BUY WAR BONDS!!</b>',
+  '<i>Dear Robert,<br>I was going to send you some stationary Anyway, but since you got greedy and asked for it, now you get nothing. i hope you learnt your lesson.<br> Love, Eunuchs 🖤</i><br>[sic]',
+  '"You deserve punishment..."',
+  'Why are we still standing here looking around? Click the links.',
+  'Don\'t forget to submit a question on the questions tab.',
+  'Not optimized for mobile. Never optimized for mobile.',
+  '"The kitchen is so dirty I just stopped using it. I got a hot plate in my room or I go to my friends to coom"',
+  'Stop caring so much',
+  'Can you believe this site uses Bootstrap??',
+  '<rainbow><b><(0_0<) <(0_0)> (>0_0)> KIRBY DANCE</b><rainbow>',
+  '<code>man door hand hook car door</code>',
+  'One div led to another',
+  '<iframe src="media/Annus_Destiny.mp4" frameborder="0"></iframe>',
+  '"I can\'t really control my hunger and i\'m autistic"',
+  '"Ey, Tony, lift ya shirt up a lil\' bit, I\'m tryna see..."',
+  'DO NOT FINISH THE GAME',
+];
+ 
+function getRandomText() {
+    const randomIndex = Math.floor(Math.random() * textSelect.length);
+    return textSelect[randomIndex];
+  }
+ 
+function loadText() {
+  const myDiv = document.getElementById('random');
+  myDiv.innerHTML= getRandomText();
+}
+ 
+window.onload = loadText;
